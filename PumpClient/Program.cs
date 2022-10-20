a2aac398eedfb386a99109cfac6ae0e921829f0e
Konstantin <muselsps@ya.ru>
2022-10-20 19:45:24 +0300

    PumpService

---
+++ PumpClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using PumpClient.PumpServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PumpClient
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            InstanceContext instanceContext = new InstanceContext(new CallbackHandler());
+            PumpServiceClient client = new PumpServiceClient(instanceContext);
+
+            client.UpdateAndCompileScriptAsync(@"C:\Users\musel\source\repos\WCFPumpService\WCFPumpService\Scripts\Sample.script");
+            client.RunScriptAsync();
+
+            Console.WriteLine("Please, Enter to exit ...");
+            Console.ReadKey(true);
+            client.Close();
+        }
+    }
+}
---
+++ WCFPumpService/IPumpServiceCallback.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFPumpService
+{
+    [ServiceContract]
+    public interface IPumpServiceCallback
+    {
+        [OperationContract]
+        void UpdateStatistic(StatisticsService statistics);
+    }
+}
